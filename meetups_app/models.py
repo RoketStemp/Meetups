10cd84b7a1e31d07851f44f6fcef94456fbe47d6
RoketStemp <stemproket@gmail.com>
2021-07-23 16:39:20 +0300

    Change description field to TextField and add upload_to attr to image field

--- meetups_app/models.py
+++ meetups_app/models.py
@@ -5,8 +5,8 @@ class Meetup(models.Model):
     title = models.CharField(max_length=255, verbose_name='Meetup title')
     slug = models.SlugField(unique=True, verbose_name='Meetup slug')
     organizer_email = models.EmailField(verbose_name='Organizer field')
-    description = models.CharField(max_length=255, verbose_name='Meetup description')
-    image = models.ImageField()
+    description = models.TextField(verbose_name='Meetup description')
+    image = models.ImageField(upload_to='images')
 
     def __str__(self):
         return f'{self.title}'
