f2712e545f45d1af846c5c25236ba0873dc030fd
RoketStemp <stemproket@gmail.com>
2021-07-23 16:28:28 +0300

    created Meetup model

--- meetups_app/models.py
+++ meetups_app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Meetup(models.Model):
+    title = models.CharField(max_length=255, verbose_name='Meetup title')
+    slug = models.SlugField(unique=True, verbose_name='Meetup slug')
+    organizer_email = models.EmailField(verbose_name='Organizer field')
+    description = models.CharField(max_length=255, verbose_name='Meetup description')
+    image = models.ImageField()
+
+    def __str__(self):
+        return f'{self.title}'
