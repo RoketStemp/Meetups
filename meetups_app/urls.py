90b08ffaec20cc2135e8420e69ad7625017e554a
RoketStemp <stemproket@gmail.com>
2021-07-27 18:14:26 +0300

    added register form

--- meetups_app/urls.py
+++ meetups_app/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('<str:slug>/success', views.confirm_registration, name='confirm-registration'),
     path('<str:slug>/', views.meetup_detail, name='meetup-detail')
 ]
--- meetups_app/views.py
+++ meetups_app/views.py
@@ -1,7 +1,9 @@
 from django.core.exceptions import ObjectDoesNotExist
-from django.shortcuts import render
 
-from .models import Meetup
+from django.shortcuts import render, redirect
+
+from .models import Meetup, Participants
+from .forms import RegistrationForm
 
 
 def index(request):
@@ -13,12 +15,48 @@ def index(request):
 
 def meetup_detail(request, slug):
     try:
-        meetup = Meetup.objects.get(slug=slug)
+        selected_meetup = Meetup.objects.get(slug=slug)
+        if request.method == 'GET':
+            registration_form = RegistrationForm()
+            return render(request, 'meetups_app/meetup-details.html', {
+                'meetup_found': True,
+                'meetup': selected_meetup,
+                'form': registration_form
+            })
+        else:
+            registration_form = RegistrationForm(request.POST)
+            if registration_form.is_valid():
+                user_email = registration_form.cleaned_data['email']
+                participant, was_created = Participants.objects.get_or_create(email=user_email)
+                if was_created:
+                    selected_meetup.participants.add(participant)
+                    return redirect('confirm-registration', slug=slug)
+                else:
+                    try:
+                        selected_meetup.participants.get(email=user_email)
+                        registration_form.add_error('email', 'U\'ve already registered to this Meetup!')
+                        return render(request, 'meetups_app/meetup-details.html', {
+                            'meetup_found': True,
+                            'meetup': selected_meetup,
+                            'form': registration_form
+                        })
+                    except ObjectDoesNotExist:
+                        selected_meetup.participants.add(participant)
+                        return redirect('confirm-registration', slug=slug)
+
         return render(request, 'meetups_app/meetup-details.html', {
-            'meetup': meetup,
-            'meetup_found': True
+            'meetup_found': True,
+            'meetup': selected_meetup,
+            'form': registration_form
         })
     except ObjectDoesNotExist:
         return render(request, 'meetups_app/meetup-details.html', {
             'meetup_found': False
         })
+
+
+def confirm_registration(request, slug):
+    meetup = Meetup.objects.get(slug=slug)
+    return render(request, 'meetups_app/registration-success.html', {
+        'organizer_email': meetup.organizer_email
+    })
