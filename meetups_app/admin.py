4a32f676ebe51d237ddef0409d9b6f51cbc8a8ae
RoketStemp <stemproket@gmail.com>
2021-07-27 15:07:07 +0300

    Modifying admin page
    Added slug-autogeneration and filter by location and changed displaying

--- meetups_app/admin.py
+++ meetups_app/admin.py
@@ -1,6 +1,14 @@
 from django.contrib import admin
 
-from .models import Meetup
+from .models import Meetup, Participants, Location
 
 
-admin.site.register(Meetup)
+class MeetupAdmin(admin.ModelAdmin):
+    list_display = ('title', 'location')
+    list_filter = ('location',)
+    prepopulated_fields = {'slug': ('title',)}
+
+
+admin.site.register(Meetup, MeetupAdmin)
+admin.site.register(Participants)
+admin.site.register(Location)
